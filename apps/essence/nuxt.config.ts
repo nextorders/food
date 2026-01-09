b20d91f53f19923d1acf71146ca817a59718d56b
Nick Kosarev <hmbanan666@hotmail.com>
2026-01-09 13:21:13 +0200

    chore: removed old parts (#442)
    * chore: removed old parts
    
    * chore: update

--- apps/essence/nuxt.config.ts
+++ apps/essence/nuxt.config.ts
@@ -1,11 +1,3 @@
 export default defineNuxtConfig({
-  extends: ['@nextorders/core'],
-  typescript: {
-    tsConfig: {
-      compilerOptions: {
-        erasableSyntaxOnly: true,
-      },
-    },
-  },
   compatibilityDate: '2025-11-20',
 })
